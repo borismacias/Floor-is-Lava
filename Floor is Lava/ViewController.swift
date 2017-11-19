1901478dcdf63fc75adc08515f0ec615125de89e
Boris <bgonzalez@ing.uchile.cl>
2017-11-19 16:38:45 -0300

    :tada:

---
+++ Floor is Lava/ViewController.swift
@@ -0,0 +1,67 @@
+//
+//  ViewController.swift
+//  Floor is Lava
+//
+//  Created by Boris Alexis Gonzalez Macias on 11/19/17.
+//  Copyright © 2017 PantlessDev. All rights reserved.
+//
+
+import UIKit
+import ARKit
+
+class ViewController: UIViewController, ARSCNViewDelegate {
+
+    @IBOutlet weak var sceneView: ARSCNView!
+    
+    let configuration = ARWorldTrackingConfiguration()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints, ARSCNDebugOptions.showWorldOrigin]
+        self.sceneView.session.run(configuration)
+        self.configuration.planeDetection = .horizontal
+        self.sceneView.delegate = self
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func createLava(planeAnchor: ARPlaneAnchor) -> SCNNode {
+        let lavaNode = SCNNode(geometry: SCNPlane(width: CGFloat(planeAnchor.extent.x), height: CGFloat(planeAnchor.extent.z)))
+        lavaNode.geometry?.firstMaterial?.diffuse.contents = #imageLiteral(resourceName: "Lava")
+        lavaNode.geometry?.firstMaterial?.isDoubleSided = true
+        lavaNode.position = SCNVector3( planeAnchor.center.x, planeAnchor.center.y, planeAnchor.center.z )
+        lavaNode.eulerAngles = SCNVector3(90.degreesToRadians, 0, 0)
+        return lavaNode
+    }
+    
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        guard let planeAnchor = anchor as? ARPlaneAnchor else { return }
+        let lavaNode = createLava(planeAnchor: planeAnchor)
+        node.addChildNode(lavaNode)
+    }
+
+    func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
+        guard let planeAnchor = anchor as? ARPlaneAnchor else { return }
+        node.enumerateChildNodes { (node, _) in
+            node.removeFromParentNode()
+        }
+        
+        let lavaNode = createLava(planeAnchor: planeAnchor)
+        node.addChildNode(lavaNode)
+    }
+    
+    func renderer(_ renderer: SCNSceneRenderer, didRemove node: SCNNode, for anchor: ARAnchor) {
+        guard let _ = anchor as? ARPlaneAnchor else { return }
+        node.enumerateChildNodes { (node, _) in
+            node.removeFromParentNode()
+        }
+    }
+}
+
+extension Int {
+    var degreesToRadians: Double { return Double(self) * .pi/180 }
+}
+
